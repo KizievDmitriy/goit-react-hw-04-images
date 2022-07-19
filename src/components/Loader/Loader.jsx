61f753187ca24df458f6b0ec25921aa53c1696a5
KizievDmitriy <kizievdmitrii@gmail.com>
2022-07-19 17:28:01 +0300

    add files

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import { TailSpin } from  'react-loader-spinner'
+export const Loader = () => {
+  return (
+    <TailSpin
+    height="100"
+    width="100"
+    color='#3f51b5'
+    ariaLabel='loading'
+  />
+  );
+};
